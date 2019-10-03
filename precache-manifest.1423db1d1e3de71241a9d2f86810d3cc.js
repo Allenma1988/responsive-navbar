43a31f5ed913136e0ca71c2872c02353fa430da2
allenma1988 <allenma1988@gmail.com>
2019-10-02 22:20:14 -0700

    Updates

---
+++ precache-manifest.1423db1d1e3de71241a9d2f86810d3cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cb1097070b2ab3192c5a161e65ec54",
+    "url": "/responsive-navbar/index.html"
+  },
+  {
+    "revision": "ed950973b56af0e1cfff",
+    "url": "/responsive-navbar/static/css/main.15bb9530.chunk.css"
+  },
+  {
+    "revision": "156e2ab6847374a1bcb3",
+    "url": "/responsive-navbar/static/js/2.e27de1d7.chunk.js"
+  },
+  {
+    "revision": "ed950973b56af0e1cfff",
+    "url": "/responsive-navbar/static/js/main.788fef80.chunk.js"
+  },
+  {
+    "revision": "5494cbc81233308c81c9",
+    "url": "/responsive-navbar/static/js/runtime-main.15d8a6b4.js"
+  }
+]);
